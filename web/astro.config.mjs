4546e12a17adcb164a5c9ebb9bb3178b076c5fdf
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-04-28 11:59:32 +0200

    feat: v1.2

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,10 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   output: 'static',
   outDir: '../pocketbase/pb_public',
-  trailingSlash: 'never',
-  build: {
-    format: 'file',
-  },
   vite: {
     plugins: [tailwindcss()],
   },
